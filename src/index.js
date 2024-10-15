50a0c31889c3ca35f43fc8da55b6013e66b1df67
sarahmsoh <sarahmuriithi523@gmail.com>
2024-10-15 10:19:23 +0300

    added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const formSubmission = document.getElementById("create-task-form");
+
+formSubmission.addEventListener("submit", submissionEvent);
+
+function submissionEvent(event) {
+  event.preventDefault();
+  const newTaskDescription = event.target["new-task-description"].value;
+  console.log(newTaskDescription);
+  handleTasks(newTaskDescription);
+}
+
+function handleTasks(toDo) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.textContent = "cancel";
+  btn.addEventListener("click", () => p.remove());
+  p.textContent = toDo;
+  p.appendChild(btn);
+
+  document.querySelector("#list").appendChild(p);
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Dom functions appropriatelly and list functions well")
+});
 });
